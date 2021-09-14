88ee9a7da104528e5383c44a3c51dd7d4196adeb
Roderic Russell <rrr5458@gmail.com>
2021-09-14 15:12:03 -0400

    rendering items functional

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
+import ItemDetails from './components/ItemDetails';
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
           </Route>
           <Route exact path="/dashboard">
             <DashboardPage />
+          </Route>
+          <Route exact path="/itemdetails">
+            <ItemDetails />
           </Route>
         </Switch>
       </div>
