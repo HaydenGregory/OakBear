66a4617e6e7aa4dbca4f6ef4add24695bca3a858
Hayden Gregory <gregory00123@gmail.com>
2021-09-14 11:11:48 -0500

    added route to get user info and connected to dash

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './DashboardPage.css'
 
 function DashboardPage() {
 
     const [tabClick, setTabClick] = useState("Sold")
+    const [editing, setEditing] = useState(false)
+    const [userInfo, setUserInfo] = useState("")
 
+    useEffect(() => {
+        fetch('/user/getuser').then(res => res.json()).then(userData => {
+            setUserInfo(userData)
+        })
+    }, [])
+
+    function handleUserInfoChange(){
+        setEditing(false)
+        // create a new user copy
+        const userUpdate = {...userInfo} 
+
+        // send fetch with new user to backend ('/user/update')
+    }
+
+    const { bio, name, gender, preferences } = userInfo
     return (
         <div className="dashboard-container-main">
             <div className="picture-bio">
@@ -12,22 +29,33 @@ function DashboardPage() {
                     <img class="profile-bear" alt="user-img" src='/Images/Profile.png' />
                 </div>
                 <div className="bio-container">
-                    <span>Hello this is a sample bio </span>
+                    <h2>
+                        {name}
+                        <button onClick={() => setEditing(true)} className='edit-button'>
+                            <img alt='edit-icon' class="edit-icon" src="/Images/edit-icon.png" />
+                        </button>
+                    </h2>
+                    <h4>{gender}</h4>
+                    <span>{bio}</span>
+                    {editing ? <button onClick={() => handleUserInfoChange()}>Submit Changes</button> : ''}
                 </div>
             </div>
             <div className="pref-listed">
                 <div className="preferences">
                     <span>Preferences</span>
+                    <ul>
+                        <li>{preferences}</li>
+                    </ul>
                 </div>
                 <div className="change-table">
                     <div class="pagination">
-                        <button class={tabClick === "Sold" && "active"} onClick={()=>setTabClick("Sold")}>Sold</button>
-                        <button class={tabClick === "Purchased" && "active"} onClick={()=>setTabClick("Purchased")}>Purchased</button>
-                        <button class={tabClick === "Saved" && "active"} onClick={()=>setTabClick("Saved")}>Saved</button><br /><br />
+                        <button class={tabClick === "Sold" && "active"} onClick={() => setTabClick("Sold")}>Sold</button>
+                        <button class={tabClick === "Purchased" && "active"} onClick={() => setTabClick("Purchased")}>Purchased</button>
+                        <button class={tabClick === "Saved" && "active"} onClick={() => setTabClick("Saved")}>Saved</button><br /><br />
                         <div>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </p>
+                            {tabClick === "Sold" && <p>Test for sold tab</p>}
+                            {tabClick === "Purchased" && <p>Test for Purchased tab</p>}
+                            {tabClick === "Saved" && <p>Test for Saved tab</p>}
                         </div>
                     </div>
                 </div>
